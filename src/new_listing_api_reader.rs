aa478c8ed6683dfee69643c349f6729c69e03d80
Enigo <unknown>
2023-01-07 16:02:25 +0800

    Improved response processing
    * Better multithreading execution
    * Better error handling
    * Image file name is now randomly generated

--- src/new_listing_api_reader.rs
+++ src/new_listing_api_reader.rs
@@ -1,5 +1,7 @@
 use chrono::{DateTime, Duration, Utc};
-use log::info;
+use futures::future::join_all;
+use log::{error, info};
+use tokio::task;
 
 use crate::model::{order::*, token::Token};
 use crate::telegram_bot_sender;
@@ -10,53 +12,54 @@ const ORDERS_URL: &str = "https://api.x.immutable.com/v1/orders?status=active&se
 const TOKEN_URL: &str = "https://api.x.immutable.com/v1/assets/0x9e0d99b864e1ac12565125c5a82b59adea5a09cd/";
 
 #[tokio::main]
-pub async fn read_orders() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn read_orders() {
     let response = reqwest::get(ORDERS_URL)
-        .await?
-        .text()
-        .await?;
+        .await.unwrap().text()
+        .await.unwrap();
 
     let result = serde_json::from_str::<Order>(&response);
 
     match result {
         Ok(order) => {
-            return process_order(order)
-                .await;
+            info!("Processing order response");
+            let mut futures = vec![];
+            for result in order.result {
+                let future = task::spawn(process_order(result));
+                futures.push(future);
+            }
+
+            join_all(futures).await;
         }
         Err(e) => {
-            panic!("Orders API response cannot be parsed! {}", e)
+            error!("Orders API response cannot be parsed! {}", e)
         }
     };
 }
 
-async fn process_order(order: Order) -> Result<(), Box<dyn std::error::Error>> {
-    info!("Processing order response");
-    for result in order.result {
-        let timestamp = DateTime::parse_from_rfc3339(&result.timestamp).unwrap();
-        let for_last_minutes = 1;
-        let last_minute = Utc::now() - Duration::minutes(for_last_minutes);
-        let is_after = timestamp > last_minute;
+async fn process_order(result: TheResult) {
+    let timestamp = DateTime::parse_from_rfc3339(&result.timestamp).unwrap();
+    let for_last_minutes = 1;
+    let last_minute = Utc::now() - Duration::minutes(for_last_minutes);
+    let is_after = timestamp > last_minute;
 
-        if is_after {
-            let response = reqwest::get(TOKEN_URL.to_owned() + &result.sell.data.id)
-                .await?
-                .text()
-                .await?;
-            let parse_result = serde_json::from_str::<Token>(&response);
+    if is_after {
+        info!("Newly listed land detected");
+        let response = reqwest::get(TOKEN_URL.to_owned() + &result.sell.data.id)
+            .await.unwrap()
+            .text().await.unwrap();
+        let parse_result = serde_json::from_str::<Token>(&response);
 
-            match parse_result {
-                Ok(token) => {
-                    let buy = result.buy;
-                    telegram_bot_sender::send(token.metadata, get_price(buy.data), buy.the_type)
-                        .await?;
-                }
-                Err(e) => {
-                    panic!("Token API response cannot be parsed! {}", e)
-                }
-            };
-        }
+        match parse_result {
+            Ok(token) => {
+                let buy = result.buy;
+                telegram_bot_sender::send(token.metadata, get_price(buy.data), buy.the_type)
+                    .await;
+            }
+            Err(e) => {
+                error!("Token API response cannot be parsed! {}", e)
+            }
+        };
     }
-    Ok(())
 }
 
 fn get_price(data: BuyData) -> f32 {
--- src/telegram_bot_sender.rs
+++ src/telegram_bot_sender.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
-use log::info;
+
+use log::{error, info, warn};
 use reqwest::StatusCode;
-use resvg::{tiny_skia, usvg::{Options, Tree, FitTo}};
+use resvg::{tiny_skia, usvg::{FitTo, Options, Tree}};
 use teloxide::prelude::*;
 use teloxide::types::InputFile;
+
+use rand::distributions::{Alphanumeric, DistString};
 
 use crate::model::token::Metadata;
 
@@ -13,39 +16,42 @@ const USER_AGENT_VALUE: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.
 const BOT_TOKEN: &str = "<>"; // insert correct one
 const CHAT_ID: i64 = 123; // insert correct one
 
-pub async fn send(metadata: Metadata, price: f32, buy_type: String) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn send(metadata: Metadata, price: f32, buy_type: String) {
+    let image_url = reqwest::Url::parse(&metadata.image_url).unwrap();
+    let client = reqwest::Client::new();
+    let response = client.get(image_url)
+        .header(reqwest::header::USER_AGENT, USER_AGENT_VALUE)
+        .send().await.unwrap();
+    let status_code = response.status();
+
+    if status_code != StatusCode::OK {
+        error!("Response code wasn't OK but {}", status_code)
+    } else {
+        let img_text = &response.text().await.unwrap();
+        let file_name = &format!("/tmp/{}.png", Alphanumeric.sample_string(&mut rand::thread_rng(), 16));
+        let path = &Path::new(file_name);
+
+        generate_image(&img_text, path);
+
+        let message = build_message(&metadata, &price, buy_type);
+
+        let bot = Bot::new(BOT_TOKEN);
+        info!("Sending photo to telegram");
+        match bot.send_photo(ChatId(CHAT_ID), InputFile::file(path)).caption(message).await
+        {
+            Ok(message) => info!("Sent successfully {:?}", message.id),
+            Err(e) => warn!("Photo wasn't sent because of: {}", e)
+        };
+        fs::remove_file(path).unwrap();
+    }
+}
+
+fn build_message(metadata: &Metadata, price: &f32, buy_type: String) -> String {
     let token_type = HashMap::from([
         (String::from("ERC20"), String::from("USDC"))
     ]);
     let token = token_type.get(&buy_type).map_or("ETH", String::as_str);
 
-    let bot = Bot::new(BOT_TOKEN);
-
-    let image_url = reqwest::Url::parse(&metadata.image_url).unwrap();
-    let client = reqwest::Client::new();
-    let response = client.get(image_url)
-        .header(reqwest::header::USER_AGENT, USER_AGENT_VALUE)
-        .send().await?;
-    let status_code = response.status();
-
-    if status_code != StatusCode::OK {
-        panic!("Response code wasn't OK but {}", status_code)
-    }
-
-    let img_text = &response.text().await?;
-    let path = &Path::new("/tmp/image.png");
-
-    generate_image(&img_text, path);
-    let message = build_message(&metadata, &price, token);
-
-    info!("Sending photo to telegram");
-    bot.send_photo(ChatId(CHAT_ID), InputFile::file(path)).caption(message).await?;
-    fs::remove_file(path)?;
-
-    Ok(())
-}
-
-fn build_message(metadata: &Metadata, price: &f32, token: &str) -> String {
     "T".to_owned() + &metadata.tier.to_string() + " " + &metadata.name
         + " price: " + &price.to_string() + token + " landmark: " + &metadata.landmark
         + " hydrogen:" + &metadata.hydrogen.to_string() + " carbon:" + &metadata.carbon.to_string()
